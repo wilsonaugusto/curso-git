8558604f0b564461aeeb58f3346112d4f85e0df2
Wilson Augusto <wilson.augusto26@gmail.com>
2020-04-21 16:25:35 -0300

    Acrescentada a pergunta sobre como vc esta hoje

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace TesteGithub {
         static void Main(string[] args) {
             Console.WriteLine("Ola Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como vc está?");
 
             Console.WriteLine();
         }
