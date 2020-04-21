42e84b1c85cc015b8dcf364dcd43f85474b9e4f8
Wilson Augusto <wilson.augusto26@gmail.com>
2020-04-21 16:05:33 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TesteGithub {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ola Mundo!");
+
+            Console.WriteLine();
+        }
+    }
+}
