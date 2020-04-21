9c449aff9dfc1a28cd15ea34dda15b0d059247d3
Wilson Augusto <wilson.augusto26@gmail.com>
2020-04-21 16:11:35 -0300

    Acrestentada uma mensagem de bom dia

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace TesteGithub {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Bom dia!");
 
             Console.WriteLine();
         }
